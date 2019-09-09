4be13922a0a26fa82c3f593f952922e49cb7c82b
Ranajit <paltua@gmail.com>
2019-09-09 22:50:29 +0530

    added

---
+++ application/controllers/api/Programme.php
@@ -0,0 +1,98 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+//To Solve File REST_Controller not found
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+class Programme extends CI_Controller {
+    use REST_Controller { REST_Controller::__construct as private __resTraitConstruct; }
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->__resTraitConstruct();
+
+        // Configure limits on our controller methods
+        // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
+        $this->methods['add_post']['limit'] = 500; // 500 requests per hour per user/key
+        $this->methods['update_post']['limit'] = 500; // 100 requests per hour per user/key
+        $this->methods['single_post']['limit'] = 500; // 100 requests per hour per user/key
+        $this->methods['list_post']['limit'] = 500; // 100 requests per hour per user/key
+        $this->load->model('tbl_generic_model');
+    }
+
+    public function list_post(){
+        $where = array();
+        $select = '*';
+        $orderBy['program_id'] = 'DESC';
+        $data = $this->tbl_generic_model->get('programs',$select, $where, $orderBy);
+        $responseData = [
+            'status' => 'success',
+            'message' => count($data) > 0?'':'No data please.',
+            'data' => $data
+        ];
+        // $retData = AUTHORIZATION::generateToken($responseData);
+        $this->response($responseData,  200); // OK (200) being the HTTP response code
+    }
+
+    public function add_post(){
+        $where = array();
+        $select = '*';
+        $orderBy['name'] = 'ASC';
+        $data = $this->tbl_generic_model->get('countries',$select, $where, $orderBy);
+        $responseData = [
+            'status' => 'success',
+            'message' => count($data) > 0?'':'No data please.',
+            'data' => $data
+        ];
+        // $retData = AUTHORIZATION::generateToken($responseData);
+        $this->response($responseData,  200); // OK (200) being the HTTP response code
+    }
+
+    public function update_post(){
+        // India 96
+        $where['country_id'] = $this->uri->segment(4);
+        $select = '*';
+        $orderBy['name'] = 'ASC';
+        $data = $this->tbl_generic_model->get('regions',$select, $where, $orderBy);
+        $responseData = [
+            'status' => 'success',
+            'message' => count($data) > 0?'':'No data please.',
+            'data' => $data
+        ];
+        // $retData = AUTHORIZATION::generateToken($responseData);
+        $this->response($responseData,  200); // OK (200) being the HTTP response code
+    }
+
+    public function single_post(){
+        // West Bengal 1627
+        $where['region_id'] = $this->uri->segment(4);
+        $select = '*';
+        $orderBy['name'] = 'ASC';
+        $data = $this->tbl_generic_model->get('cities',$select, $where, $orderBy);
+        $responseData = [
+            'status' => 'success',
+            'message' => count($data) > 0?'':'No data please.',
+            'data' => $data
+        ];
+        // $retData = AUTHORIZATION::generateToken($responseData);
+        $this->response($responseData,  200); // OK (200) being the HTTP response code
+    }
+
+}
